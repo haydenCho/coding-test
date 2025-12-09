bd7b92a32e766b0ed02d22a30ce29fba5eabd4a4
EunsolnCho <cho_eunsol@naver.com>
2025-12-09 17:09:01 +0900

    파이썬 알고리즘 인터뷰-유효한 팰린드롬

---
+++ Python-Coding-Interview/06.문자열_조작/01.유효한_팰린드롬(125).py
@@ -0,0 +1,48 @@
+# 리스트로 풀이
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        strs = []
+
+        # 리스트에 소문자 담기
+        for i in s:
+            if i.isalnum():
+                strs.append(i.lower())
+
+        # 팰린드롬 확인
+        while(len(strs) > 1):
+            left = strs.pop(0)
+            right = strs.pop()
+            if(left!=right):
+                return False
+        
+        return True
+      
+# ======================================================
+# 데크로 풀이
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # 데크 자료형으로 생성
+        strs: Deque = collections.deque()
+
+        # 리스트에 소문자 담기
+        for i in s:
+            if i.isalnum():
+                strs.append(i.lower())
+
+        # 팰린드롬 확인
+        while len(strs) > 1:
+            if strs.popleft() != strs.pop():
+                return False
+        
+        return True
+
+# ======================================================
+# 슬라이싱으로 풀이
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+
+        # 정규식으로 불필요한 문자 필터링
+        s = re.sub('[^a-z0-9]', '', s)
+
+        return s == s[::-1]     # 슬라이싱
---
+++ Python-Coding-Interview/06.문자열_조작/README.md
@@ -0,0 +1,13 @@
+## 문자열 조작
+### 문자열 관련 파이썬 메서드
+- upper(): 문자열을 대문자로 변환
+- lower(): 문자열을 소문자로 변환
+- isalnum(): 문자열이 알파벳 또는 숫자로만 구성되어 있으면 True, 아니면 False를 반환
+- isalpha(): 문자열이 알파벳으로만 구성되어 있으면 True, 아니면 False를 반환
+- isdecimal(): 문자열이 정수이면 True, 아니면 False를 반환
+- isdigit(): 문자열이 숫자이면 True, 아니면 False를 반환
+- isspace(): 문자열이 공백으로만 구성되어 있으면 True, 아니면 False를 반환
+
+### 정규식
+- (위키독스)[https://wikidocs.net/4308]
+- (참고 블로그)[https://velog.io/@euisuk-chung/%ED%8C%8C%EC%9D%B4%EC%8D%AC-%EC%A0%95%EA%B7%9C%ED%91%9C%ED%98%84%EC%8B%9D-%ED%99%9C%EC%9A%A9-%EB%B0%A9%EB%B2%95]
