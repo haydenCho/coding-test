45848a6c09dc6b1d2d54e5f6cb7e3834cbcad2de
EunsolnCho <cho_eunsol@naver.com>
2026-02-05 15:14:20 +0900

    파이썬 알고리즘 인터뷰-보석과 돌

--- Python-Coding-Interview/11.해시테이블/29.보석과_돌(771).py
+++ Python-Coding-Interview/11.해시테이블/29.보석과_돌(771).py
@@ -19,8 +19,24 @@ class Solution:
         return count
 
 # ==================================================
-# 
+# defaultdict를 이용한 비교 전략
 '''
-
+- 실행속도는 비슷하고 코드 길이는 많이 줄었음
 
 '''
+import collections
+
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        freqs = collections.defaultdict(int)
+        count = 0
+
+        # 돌(S)의 빈도 수 계산
+        for char in stones:
+            freqs[char] += 1
+        
+        # 보석(J)의 빈도 수 합산
+        for char in jewels:
+            count += freqs[char]
+
+        return count
