73c1795d264f7a0f68e524da5eaa21c5e43e56ec
EunsolnCho <cho_eunsol@naver.com>
2026-02-06 15:06:53 +0900

    파이썬 알고리즘 인터뷰-보석과 돌

--- Python-Coding-Interview/11.해시테이블/29.보석과_돌(771).py
+++ Python-Coding-Interview/11.해시테이블/29.보석과_돌(771).py
@@ -40,3 +40,21 @@ class Solution:
             count += freqs[char]
 
         return count
+
+# ==================================================
+# Counter로 계산 생략
+'''
+- Counter를 사용하면 개수를 계산하는 과정을 자동으로 처리할 수 있다.
+- 빈도 수와 관련된 문제가 나온다면 Counter를 생각하기
+
+'''
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        freqs = collections.Counter(stones)     # 돌(sttones)의 빈도 수 계산
+        count = 0
+
+        # 비교 없이 보석(J)의 빈도 수 합산
+        for char in jewels:
+            count += freqs[char]
+
+        return count
